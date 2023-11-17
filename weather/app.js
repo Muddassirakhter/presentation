557d8b2bc73950b7585b6915ac3c48c49f4903d3
Muddassirakhter <137832004+Muddassirakhter@users.noreply.github.com>
2023-11-17 21:25:13 +0500

    Add files via upload

---
+++ weather/app.js
@@ -0,0 +1,31 @@
+async function getWeather(event) {
+
+    event.preventDefault()
+
+    let API_KEY = "26ae9a0b27a97215beaa4b5df9aa15fb"
+
+    let cityName = document.querySelector("#search").value
+
+    try {
+
+        
+        let response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`);
+
+
+        console.log(response.data);
+ document.querySelector("#p").innerText = 
+ 
+ `city name : ${ response.data.name }
+ weather: ${response.data.main.temp} °C 
+ Min Temperature:${ response.data.main.temp_min}
+ Max Temperature:${ response.data.main.temp_max}
+ `
+//  document.querySelector("#weather").innerText = response.data.main.temp
+//  document.querySelector("#a").innerText = response.data.main.humidity
+//  document.querySelector("#b").innerText = response.data.main.temp_min
+//  document.querySelector("#c").innerText = response.data.main.temp_max
+    } catch (error) {
+        console.log(error);
+    }
+
+}
